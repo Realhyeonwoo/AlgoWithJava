47719c8473319b7ce5f8313180df3bf8be820cb4
RealHyeonwoo <gusdn7269@naver.com>
2022-01-09 09:46:16 +0900

    boj1406

--- src/boj1406.java
+++ src/boj1406.java
@@ -12,29 +12,23 @@ public class boj1406 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		
+
 		String str = br.readLine();
 		LinkedList<Character> list = new LinkedList<>();
 		ListIterator<Character> iter = list.listIterator();
-		
 		for(int i=0; i<str.length(); i++)
 			iter.add(str.charAt(i));
-		
-//		for(int i=0; i<str.length(); i++)
-//			list.add(str.charAt(i));
-//		int idx = list.size();
+
 		int M = Integer.parseInt(br.readLine());
 		for(int i=0; i<M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String order = st.nextToken();
 			switch(order) {
 			case "L":
-//				if(idx != 0) idx--;
 				if(iter.hasPrevious()) 
 					iter.previous();
 				break;
 			case "D":
-//				if(idx != list.size()) idx++;
 				if(iter.hasNext())
 					iter.next();
 				break;
@@ -43,14 +37,8 @@ public class boj1406 {
 					iter.previous();
 					iter.remove();
 				}
-//				if(idx != 0) {
-//					list.remove(idx-1);
-//					idx--;
-//				}
 				break;
 			case "P":
-//				list.add(idx, st.nextToken().charAt(0));
-//				idx++;
 				iter.add(st.nextToken().charAt(0));
 				break;
 				default:
@@ -58,12 +46,9 @@ public class boj1406 {
 		}
 		
 		for(char v : list)
-			bw.write(v);
+			sb.append(v);
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
-//		
-//		bw.write(sb.toString());
-//		bw.flush();
-//		bw.close();
 	}
 }
