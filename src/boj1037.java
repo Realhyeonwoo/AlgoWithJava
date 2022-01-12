fd8949a699f79beff6d658c0550664205399cf6e
Realhyeonwoo <gusdn7269@naver.com>
2022-01-12 23:01:05 +0900

    boj1037

---
+++ src/boj1037.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class boj1037 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i<N; i++)
+			list.add(Integer.parseInt(st.nextToken()));
+		Collections.sort(list);
+		
+		System.out.println(list.get(0) * list.get(list.size()-1));
+	}
+}
