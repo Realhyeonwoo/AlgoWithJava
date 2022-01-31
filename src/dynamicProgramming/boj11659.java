8e399e0692c73a5d77fdd12368ec896fc899596c
RealHyeonwoo <gusdn7269@naver.com>
2022-01-31 14:56:38 +0900

    boj11659

---
+++ src/dynamicProgramming/boj11659.java
@@ -0,0 +1,39 @@
+package dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj11659 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N+1];
+		int[] dp = new int[1001];
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = dp[i-1] + arr[i];
+		}
+		
+		for(int m=0; m<M; m++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			
+			sb.append(dp[j]-dp[i-1] + "\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		
+	}
+}
