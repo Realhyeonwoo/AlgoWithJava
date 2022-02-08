b24c8b9bc178aeffe6df5b1fd71e18e51beb3390
Realhyeonwoo <gusdn7269@naver.com>
2022-02-08 23:41:13 +0900

    boj2910

--- src/sort/boj5648.java
+++ src/sort/boj5648.java
@@ -4,17 +4,54 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class boj5648 {
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner sc = new Scanner(System.in);
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr;
+		int N = sc.nextInt();
+		String[] arr;
+		arr = new String[N];
+		for(int i=0; i<N; i++)
+			arr[i] = sc.next();
 		
-		int n = Integer.parseInt(st.nextToken());
+		for(int i=0; i<N; i++) {
+			String str = arr[i];
+			String temp = "";
+			boolean isZero = false;
+			for(int j=str.length()-1; j>=0; j--) {
+				if(isZero && str.charAt(j) == '0') {
+					temp += str.charAt(j);
+				}
+				if(str.charAt(j) != '0') {
+					isZero = true;
+					temp += str.charAt(j)+"";
+				}
+			}
+			arr[i] = temp;
+		}
+		
+		int[] num = new int[N];
+		for(int i=0; i<N; i++) {
+			num[i] = Integer.parseInt(arr[i]);
+		}
+		Arrays.sort(num);
+		
+		for(int i=0; i<num.length; i++)
+			System.out.println(num[i]);
+//		StringBuilder sb = new StringBuilder();
+//		for(int v : num)
+//			sb.append(v + "\n");
+//		
+//		sc.close();
+//		bw.write(sb.toString());
+//		bw.flush();
+//		bw.close();
+		
 		
 	}
 }
