1522837f98e94dcfb684180bc3feb2f12d132114
Realhyeonwoo <gusdn7269@naver.com>
2022-02-08 21:29:52 +0900

    boj11656

---
+++ src/sort/boj5648.java
@@ -0,0 +1,20 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj5648 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr;
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+	}
+}
