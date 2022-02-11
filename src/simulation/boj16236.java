a545f3c7943c666f6a09584918d2ef6d22a960f6
Realhyeonwoo <gusdn7269@naver.com>
2022-02-11 10:04:35 +0900

    boj16236

--- src/simulation/boj16236.java
+++ src/simulation/boj16236.java
@@ -23,6 +23,9 @@ public class boj16236 {
 			this.x = x;
 		}
 	}
+	static int[] dy = {0, 0, 1, -1};
+	static int[] dx = {1, -1, 0, 0};
+	
 	static int N;
 	static int[][] map;
 	static Fish shark;
@@ -45,29 +48,76 @@ public class boj16236 {
 		
 		int ans = 0;
 		while(true) {
-			boolean isMove = false;
-			int time = Integer.MAX_VALUE;
+			boolean isEat = false;
+			Fish tempShark = new Fish(shark.y, shark.x, shark.size);
+			Pos tempFish = new Pos(0, 0);
+			int dist = Integer.MAX_VALUE;
+
 			for(int y=0; y<N; y++) {
 				for(int x=0; x<N; x++) {
-					if(map[y][x] > shark.size) continue;
-					Fish cloneShark = new Fish(shark.y, shark.x, shark.size);
-					int[][] cloneMap = new int[N][N];
+					if(map[y][x] == 0) continue;
+					if(map[y][x] >= shark.size) continue;
+
+					int[][] visited = new int[N][N];
 					for(int i=0; i<N; i++)
-						cloneMap[i] = map[i].clone();
+						Arrays.fill(visited[i], -1);
+					Queue<Pos> q = new LinkedList<>();
 					
-					int cnt = bfs(y, x, cloneShark, cloneMap);
-					if(cnt != -1) { 
-						time = Math.min(time, cnt);
-						isMove = true;
-						eatCnt++;
-						int sharkSize = shark.size;
-						if(eatCnt == sharkSize) {
+					q.add(new Pos(shark.y, shark.x));
+					visited[shark.y][shark.x] = 0;
+					while(!q.isEmpty()) {
+						Pos p = q.poll();
+						if(p.y == y && p.x == x) {
+							if(dist > visited[y][x]) {
+								dist = visited[y][x];
+								tempFish = new Pos(y, x);
+								int sharkSize = shark.size;
+								if(eatCnt+1 == sharkSize) sharkSize++;
+								tempShark = new Fish(y, x, sharkSize);
+								isEat = true;
+								break;
+							}
+						}
+						for(int dir=0; dir<4; dir++) {
+							int ny = p.y + dy[dir];
+							int nx = p.x + dx[dir];
+							
+							if(ny<0 || ny>=N || nx<0 || nx>=N) continue;
+							if(visited[ny][nx] != -1) continue;
+							if(map[ny][nx] > shark.size) continue;
+							
+							visited[ny][nx] = visited[p.y][p.x] + 1;
+							q.add(new Pos(ny, nx));
 						}
 					}
 				}
 			}
-			if(!isMove) break;
-			ans = time;
+			
+			if(!isEat) break;
+			else {
+				eatCnt++;
+				int size = shark.size;
+				if(shark.size == eatCnt) {
+					eatCnt = 0;
+					size++;
+				}
+				shark = new Fish(tempShark.y, tempShark.x, size);
+				map[tempFish.y][tempFish.x] = 0; 
+				ans += dist;
+				
+//				System.out.println("DIST : " + dist + " EAT : " + eatCnt);
+//				System.out.println("SHARK : " + shark.y + " " + shark.x + " " + shark.size);
+//				for(int y=0; y<N; y++) {
+//					for(int x=0; x<N; x++) {
+//						System.out.print(map[y][x] + " ");
+//					}
+//					System.out.println();
+//				}
+//				System.out.println();
+				
+			}
+			
+			
 		}
 		
 		System.out.println(ans);
@@ -76,22 +126,7 @@ public class boj16236 {
 	}
 
 	private static int bfs(int y, int x, Fish s, int[][] cloneMap) {
-		int[] dy = {0, 0, 1, -1};
-		int[] dx = {1, -1, 0, 0};
 		
-		int[][] visited = new int[N][N];
-		for(int i=0; i<N; i++)
-			Arrays.fill(visited[i], -1);
-		Queue<Pos> q = new LinkedList<>();
-		q.add(new Pos(s.y, s.x));
-		visited[s.y][s.x] = 0;
-		while(!q.isEmpty()) {
-			Pos p = q.poll();
-			if(p.y == y && p.x == x) {
-				map[y][x] = 0;
-				return visited[y][x];
-			}
-		}
 		
 		return -1;
 	}
