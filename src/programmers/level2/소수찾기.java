27852efae81b06b5636f6c213a1980ea1862f289
Realhyeonwoo <gusdn7269@naver.com>
2022-09-11 12:01:21 +0900

    프록르ㅐ머스 피로도

--- src/programmers/level2/소수찾기.java
+++ src/programmers/level2/소수찾기.java
@@ -61,7 +61,7 @@ public class 소수찾기 {
 		if(num == 0 || num == 1) return false;
 		if(num == 2) return true;
 		
-		for(int i=3; i<=(num/2); i++) {
+		for(int i=2; i<=(num/2); i++) {
 			if((num%i) == 0) return false;
 		}
 		return true;
